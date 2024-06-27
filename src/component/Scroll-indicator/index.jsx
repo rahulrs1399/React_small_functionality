1848d9e87b355ca6f50ec973d302194db0b68208
Rahul Saroj <sarojrahul1399@gmail.com>
2024-06-27 07:59:22 +0530

    update

--- src/component/Scroll-indicator/index.jsx
+++ src/component/Scroll-indicator/index.jsx
@@ -1,26 +1,34 @@
 import { useEffect, useState } from "react";
 
-export default function ScrollIndicator ({url}) {
+export default function ScrollIndicator({ url }) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(true);
 
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(true)
+  async function fetchProducts(getUrl) {
+    try {
+      setLoading(true);
+      const response = await fetch(getUrl);
+      const data = await response.json();
 
-    async function fetchProducts(getUrl){
-        try {
-            const response = await fetch(getUrl);
-            const data = await response.json();
+      // console.log(data)
 
-            console.log(data)
-            
-            
-        } catch (error) {
-            console.log(error)
-        }
+      if (data && data.products && data.products.length > 0) {
+        setData(data.products);
+        setLoading(false);
+        
+      }
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    useEffect(()=>{
-        fetchProducts(url);
-    },[url])
+  useEffect(() => {
+    fetchProducts(url);
+  }, [url]);
+
+//   console.log(loading)
 
 }
+
+
