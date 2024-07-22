f8f4ac8f49d6ea29341dc2a603657523e84865d3
Rahul Saroj <sarojrahul1399@gmail.com>
2024-07-22 23:49:37 +0530

    feature flag

---
+++ src/component/feature-flag/data.js
@@ -0,0 +1,18 @@
+
+
+const dummyApiResponse = {
+    showLightAndDarkMode : false,
+    showTicTacToe : true,
+    showRandomColorGenerator : true,
+    showAccordian : false,
+    showTreeView : true,
+}
+
+function featureFlagsDataServiceCell(){
+    return new Promise((resolve, reject) => {
+        if(dummyApiResponse) setTimeout(resolve(dummyApiResponse), 500);
+        else(reject("Error occured! Please try again."))
+    })
+}
+
+export default featureFlagsDataServiceCell;
